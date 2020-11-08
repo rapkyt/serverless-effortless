ab63ac8e5c3239c6dc03263f46a9aeb6c1e3a056
Cristian Mora <rapkyt@gmail.com>
2020-11-08 19:36:35 -0300

    Add unsubscribe endpoint

--- serverless/settings.py
+++ serverless/settings.py
@@ -3,3 +3,5 @@ import os
 # Serverless
 # ==================================================
 DYNAMODB_TABLE = os.environ.get("DYNAMODB_TABLE")
+
+UNSUBSCRIBE_SECRET = os.environ.get("UNSUBSCRIBE_SECRET")

65849fd0ced99e191cd575ef10b90d9944560cdd
Cristian <5156110+rapkyt@users.noreply.github.com>
2020-11-10 20:34:53 -0300

    Add send_email function (#5)

--- serverless/settings.py
+++ serverless/settings.py
@@ -5,3 +5,5 @@ import os
 DYNAMODB_TABLE = os.environ.get("DYNAMODB_TABLE")
 
 UNSUBSCRIBE_SECRET = os.environ.get("UNSUBSCRIBE_SECRET")
+
+MARKETING_S3_BUCKET = os.environ.get("MARKETING_S3_BUCKET")
